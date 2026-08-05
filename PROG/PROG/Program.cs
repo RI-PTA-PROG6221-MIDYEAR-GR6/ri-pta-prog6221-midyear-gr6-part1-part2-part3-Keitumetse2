07e43ed7de2c58534248de1b0cd76432f328d241
Keitumetse2 <ST10470904@rcconnect.edu.za>
2026-08-05 15:42:11 +0200

    Newcommit

--- PROG/PROG/Program.cs
+++ PROG/PROG/Program.cs
@@ -6,6 +6,7 @@ namespace Example
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Wolrd");
+            Console.WriteLine("Good bye");
         }
         }
 }

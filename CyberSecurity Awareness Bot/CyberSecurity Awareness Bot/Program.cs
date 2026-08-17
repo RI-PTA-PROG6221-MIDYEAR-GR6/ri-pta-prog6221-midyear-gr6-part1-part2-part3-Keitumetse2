23133e094e388c12b50ba3a2fa5a3408c486449d
Keitumetse2 <ST10470904@rcconnect.edu.za>
2026-08-17 14:50:36 +0200

    Change greeting to welcome message

--- CyberSecurity Awareness Bot/CyberSecurity Awareness Bot/Program.cs
+++ CyberSecurity Awareness Bot/CyberSecurity Awareness Bot/Program.cs
@@ -6,6 +6,8 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+              Console.WriteLine("Welcome to my project");
+            
         }
     }
 }

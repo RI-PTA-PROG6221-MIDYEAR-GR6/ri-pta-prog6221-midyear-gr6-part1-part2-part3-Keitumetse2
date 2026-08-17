ce31a21460a45aad3ec707f2b7a86dc1dc96288c
Keitumetse2 <ST10470904@rcconnect.edu.za>
2026-08-17 14:45:44 +0200

    initial_1

--- CyberSecurity Awareness Bot/CyberSecurity Awareness Bot/Program.cs
+++ CyberSecurity Awareness Bot/CyberSecurity Awareness Bot/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}

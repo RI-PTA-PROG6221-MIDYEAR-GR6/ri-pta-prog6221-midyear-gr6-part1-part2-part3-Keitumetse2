132f94b7b99761a2f15e1927cdb13618a7a73aa3
Keitumetse2 <ST10470904@rcconnect.edu.za>
2026-08-20 13:56:27 +0200

    Adding new class for voice greeting

---
+++ CyberSecurity Awareness chatbot/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CyberSecurity_Awareness_chatbot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CyberSecurity Awareness chatbot/VoiceGreeting.cs
@@ -0,0 +1,17 @@
+﻿using System.Media;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberSecurity_Awareness_chatbot
+{
+    public class VoiceGreeting
+    {
+        public void Voice_Greeting()
+        {
+            String file = "";
+
+        }
+    }
+}

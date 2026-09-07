1358be2af9d8b637972b92b73219995901d034e0
Keitumetse2 <ST10470904@rcconnect.edu.za>
2026-09-07 15:27:38 +0200

    Enhance chatbot introduction with user interaction
    Added responses for the 'How are you?' interaction and modified the introduction flow.

--- CyberSecurity Awareness chatbot/Introduction.cs
+++ CyberSecurity Awareness chatbot/Introduction.cs
@@ -9,23 +9,55 @@ namespace CyberSecurity_Awareness_chatbot
     public class Introduction
     {
         public string Name_User = "";
+         public List<string> HowAreYouResponses = new List<string>
+        {
+            "I'm doing great and ready to help you learn about cybersecurity",
+            "I'm doing well, Im ready to help you stay safe online.",
+            "I'm good, Lets learn some cybersecurity tips together."
+        };
+        public Random random = new Random();
         public void Start_Introduction() 
         {  
-          Ask_User_Name(); 
+            How_Are_You();
+            Ask_User_Name(); 
         }
-       
         public void Ask_User_Name()
         {
+            Chatbot chat = new Chatbot();
             Console.WriteLine();
             Console.WriteLine("///////////>>User Names<<///////////");
-            Console.WriteLine("Bot: What is your name or nickname?"); 
+            Console.Write("Bot:"); 
+            chat.slow_Typing("Before we proceed let me get your Name "); 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("You: ");
             Console.ResetColor();
             Name_User = Console.ReadLine();
-            Console.WriteLine("Bot: Nice to meet you, " + Name_User + "!");
+            Console.Write("Bot:"); 
+            chat.slow_Typing("Nice to meet you, " + Name_User + "!");
             Console.WriteLine("////////////////////////////////////");
             Console.WriteLine(); 
+        }
+        public void How_Are_You()
+        {
+            Chatbot chat = new Chatbot();
+           Console.ForegroundColor = ConsoleColor.Cyan;
+           Console.Write("Bot: ");
+           Console.ResetColor();
+           chat.slow_Typing("How are you?");
+           Console.ForegroundColor = ConsoleColor.Green;
+           Console.Write("You: ");
+           Console.ResetColor();
+
+           string answer = Console.ReadLine();
+
+           int number = random.Next(HowAreYouResponses.Count);
+
+           Console.ForegroundColor = ConsoleColor.Cyan;
+           Console.Write("Bot: ");
+           Console.ResetColor();
+           chat.slow_Typing(HowAreYouResponses[number]);
+
+           Console.WriteLine();
         }
        
     }
